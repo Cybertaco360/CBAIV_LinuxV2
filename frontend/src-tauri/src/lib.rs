bf7c1f2652e6a8cd01226ac08998256f98d3eda2
Cybertaco360 <70378yan@gmail.com>
2024-12-28 11:41:43 -0500

    Infinity App Recursion Fix

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,31 +1,8 @@
-use std::process::{Child};
-use tauri::{Manager};
-use std::sync::Mutex;
-
-fn start_flask_backend() -> std::process::Child {
-    let path = "./bin/app";
-    std::process::Command::new(path)
-        .spawn()
-        .unwrap_or_else(|err| panic!("Failed to start Flask backend at {path}: {err}"))
-}
-
-
-
+use tauri::Manager;
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let flask_child = start_flask_backend();
-
-            app.manage(Mutex::new(flask_child));
-
             Ok(())
-        })
-        .on_window_event(|_window, event| {
-            if let tauri::WindowEvent::CloseRequested { .. } = event {
-                if let Ok(mut child) = _window.state::<Mutex<Child>>().lock() {
-                    let _ = child.kill();
-                }
-            }
         })
         .run(tauri::generate_context!())
         .expect("error while running Tauri application");
