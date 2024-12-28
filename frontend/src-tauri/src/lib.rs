baef2c2d69c59ea5bce22798c2ca641b2e5f3c2f
Cybertaco360 <70378yan@gmail.com>
2024-12-28 00:46:21 -0500

    Fixed some bugs and adding Pre-Release version

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,9 +1,10 @@
-use std::process::{Command, Child};
+use std::process::{Child};
 use tauri::{Manager};
 use std::sync::Mutex;
 
 fn start_flask_backend() -> std::process::Child {
-    std::process::Command::new("./bin/app")
+    let path = "./bin/app";
+    std::process::Command::new(path)
         .spawn()
         .unwrap_or_else(|err| panic!("Failed to start Flask backend at {path}: {err}"))
 }
